c570c336c0539ee288c9f0f97abca5fbbaa1a345
mosinshaikh1 <145239604+mosinshaikh1@users.noreply.github.com>
2025-02-14 13:25:30 +0530

    style file added

---
+++ script.js
@@ -0,0 +1,25 @@
+var a=document.querySelector("#studentloginbox")
+function studentlogin(){
+
+a.addEventListener("click",function(){
+    window.location.href = "/student/studenthome.html";
+})}
+studentlogin();
+
+var b=document.querySelector("#tpologin")
+function TAndPlogin(){
+
+    b.addEventListener("click",function(){
+        window.location.href = "t&p/Tandphome.html";
+    })}
+    TAndPlogin();
+
+    var b=document.querySelector("#componyloginbox")
+function companylogin(){
+
+    b.addEventListener("click",function(){
+        window.location.href = "company/Companyhome.html";
+    })}
+     companylogin();
+    
+
